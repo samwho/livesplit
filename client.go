356b213aa1deb6d63b4a447f1684831ab837554e
Sam Rose <samwho@lbak.co.uk>
2019-07-19 15:05:51 +0100

    Rejig locking to make things actually threadsafe this time

--- client.go
+++ client.go
@@ -16,10 +16,6 @@ const (
 	timeout = time.Duration(2) * time.Second
 )
 
-var (
-	m sync.Mutex
-)
-
 type TimerPhase string
 
 const (
@@ -30,6 +26,7 @@ const (
 )
 
 type Client struct {
+	m    sync.Mutex
 	port int
 	sock net.Conn
 }
@@ -63,105 +60,152 @@ func (client *Client) Close() error {
 }
 
 func (client *Client) StartTimer() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"starttimer"})
 }
 
 func (client *Client) StartOrSplit() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"startorsplit"})
 }
 
 func (client *Client) Split() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"split"})
 }
 
 func (client *Client) Unsplit() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"unsplit"})
 }
 
 func (client *Client) SkipSplit() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"skipsplit"})
 }
 
 func (client *Client) Pause() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"pause"})
 }
 
 func (client *Client) Resume() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"resume"})
 }
 
 func (client *Client) Reset() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"reset"})
 }
 
 func (client *Client) InitGameTime() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"initgametime"})
 }
 
 func (client *Client) SetGameTime(t time.Duration) error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"setgametime", DurationToString(t)})
 }
 
 func (client *Client) SetLoadingTimes(t time.Duration) error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"setloadingtimes", DurationToString(t)})
 }
 
 func (client *Client) PauseGameTime() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"pausegametime"})
 }
 
 func (client *Client) UnpauseGameTime() error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"unpausegametime"})
 }
 
 func (client *Client) SetComparison(comparison string) error {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.send([]string{"setcomparison", comparison})
 }
 
 func (client *Client) GetDelta(comparison string) (string, error) {
-	if err := client.send([]string{"getdelta", comparison}); err != nil {
-		return "", err
-	}
-	return client.recv()
+	client.m.Lock()
+	defer client.m.Unlock()
+	return client.sendAndRecv([]string{"getdelta", comparison})
 }
 
 func (client *Client) GetLastSplitTime() (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getlastsplittime"})
 }
 
 func (client *Client) GetComparisonSplitTime() (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getcomparisonsplittime"})
 }
 
 func (client *Client) GetCurrentTime() (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getcurrenttime"})
 }
 
 func (client *Client) GetFinalTime(comparison string) (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getfinaltime", comparison})
 }
 
 func (client *Client) GetPredictedTime(comparison string) (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getpredicatedtime", comparison})
 }
 
 func (client *Client) GetBestPossibleTime() (time.Duration, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvTime([]string{"getbestpossibletime"})
 }
 
 func (client *Client) GetSplitIndex() (int, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecvInt([]string{"getsplitindex"})
 }
 
 func (client *Client) GetCurrentSplitName() (string, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecv([]string{"getcurrentsplitname"})
 }
 
 func (client *Client) GetPreviousSplitName() (string, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	return client.sendAndRecv([]string{"getprevioussplitname"})
 }
 
 func (client *Client) GetCurrentTimerPhase() (TimerPhase, error) {
+	client.m.Lock()
+	defer client.m.Unlock()
 	s, err := client.sendAndRecv([]string{"getcurrenttimerphase"})
 	if err != nil {
 		return "", err
@@ -217,9 +261,6 @@ func StringToDuration(t string) (time.Duration, error) {
 }
 
 func (client *Client) send(cmd []string) error {
-	m.Lock()
-	defer m.Unlock()
-
 	if client.sock == nil {
 		if err := client.establishConnection(); err != nil {
 			return err
@@ -251,8 +292,6 @@ func (client *Client) recv() (string, error) {
 	var s string
 	var err error
 
-	m.Lock()
-	defer m.Unlock()
 	client.sock.SetDeadline(time.Now().Add(timeout))
 	s, err = bufio.NewReader(client.sock).ReadString('\n')
 	if err != nil {
